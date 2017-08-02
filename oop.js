a00de4e83252a06c86b51228c03348603c1716f7
tope <temitope.emmanuel63@gmail.com>
2017-08-02 15:55:15 +1300

    Implement some features based on feedback

--- oop.js
+++ oop.js
@@ -2,15 +2,17 @@ class Student{
   constructor(name,level){
     this.name=name;
     this.level=level;
-    
+
   }
   getStudent(){
-    if(typeof(this.level)==="number"){
+    //checking for invalid inputs by reducing if statements
+    let newName=this.name.trim()
+    if((this.level && newName)&&(typeof(this.level)==="number")&&(typeof(newName)==="string")){
       return `Student's name:${this.name} 
             Student's level:${this.level}
             `
     }else{
-      return "Level should be of number type"
+      return "`Level` or `name` should be of `number` or `string` type and should not be empty"
     }
   }
 }
@@ -20,32 +22,38 @@ class GradeStudent extends Student{
     this.grade=score
   }
   getGrade(){
-    if(this.grade<45){
-      return( 
-      ` ${this.name} cannot be promoted to the next level
-        Reason:${this.name}'s Score is below Average Performance
-        Advice:${this.name} should repeat the current level
-        Score:${this.score}
-        Level:${this.level}
-      `)
-    }
-    if(45<this.grade && this.grade<=70){
-      return (
-      ` ${this.name} is Promoted
-        Reason:${this.name}'s Score is Above Average Performance
-        Advice:${this.name} can still do better
-        Score:${this.score}
-        New Level:${this.level+1}
-      `)
-    }
-    if(70<this.grade && this.grade<100){
-      return (
-      ` ${this.name} is double Promoted
-        Reason:${this.name}'s Score exceeds expected Performance
-        Advice:${this.name} can do better if challenge
-        Score:${this.score}
-        New Level:${this.level+2}
-      `)
+    //checking for invalid inputs
+    let newName=this.name.trim()
+    if((this.level && newName)&&(typeof(this.level)==="number")&&(typeof(newName)==="string")&&(typeof(this.score)==="number")){
+      if(this.grade<45){
+        return( 
+        ` ${this.name} cannot be promoted to the next level
+          Reason:${this.name}'s Score is below Average Performance
+          Advice:${this.name} should repeat the current level
+          Score:${this.score}
+          Level:${this.level}
+        `)
+      }
+      if(45<this.grade && this.grade<=70){
+        return (
+        ` ${this.name} is Promoted
+          Reason:${this.name}'s Score is Above Average Performance
+          Advice:${this.name} can still do better
+          Score:${this.score}
+          New Level:${this.level+1}
+        `)
+      }
+      if(70<this.grade && this.grade<100){
+        return (
+        ` ${this.name} is double Promoted
+          Reason:${this.name}'s Score exceeds expected Performance
+          Advice:${this.name} can do better if challenge
+          Score:${this.score}
+          New Level:${this.level+2}
+        `)
+      }
+    }else{
+      return "`Level` `name` or `score` should be of `number` `string` or `number` type"
     }
   }
 }
--- test/test-spec.js
+++ test/test-spec.js
@@ -4,11 +4,13 @@ const student= require("../oop.js")
 const gradeStudent= require("../oop.js")
 
 let checkError=new student.Student("doe", "1", 12)
+let checkError2=new student.Student("doe", 1, "12")
 // let jane=new student("doe",2,60)
-describe("Get student info", function() {
-	describe("check if name is a string", () => {
-		it("should return `level must be string`", () => {
-			assert.equal(checkError.getStudent(), "Level should be of number type");
+describe("Invalid input(s)", function() {
+	describe("You have made an invalid input", () => {
+		it("should return '`Level` or `name` should be of `number` or `string` type and should not be empty'", () => {
+			assert.equal(checkError.getStudent(), "`Level` or `name` should be of `number` or `string` type and should not be empty");
 		});
 	});
 })
+
