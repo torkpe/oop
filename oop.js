fec628c62e2ea42936dc335e2c574d53673950b4
tope <temitope.emmanuel63@gmail.com>
2017-08-02 20:39:01 +0100

    Refactor code

--- oop.js
+++ oop.js
@@ -2,16 +2,6 @@ class Student{
   constructor(name,level){
     this.name=name;
     this.level=level;
-
-  }
-  getStudent(){
-    //checking for invalid inputs by reducing if statements
-    let newName=this.name.trim()//remove whitespace from beginning of string
-    if((this.level && newName)&&(typeof(this.level)==="number")&&(typeof(newName)==="string")){
-      return `Student's name:${this.name}\nStudent's level:${this.level}`
-    }else{
-      return "`Level` or `name` should be of `number` or `string` type and should not be empty"
-    }
   }
 }
 class GradeStudent extends Student{
@@ -19,21 +9,22 @@ class GradeStudent extends Student{
     super(name,level);
     this.grade=score
   }
+  getDetails(){
+    return(`${this.name} is a level ${this.level} student`)
+  }
   getGrade(){
     //checking for invalid inputs
     let newName=this.name.trim() //remove white space from beinning of string
-    if((this.level && newName &&this.grade)&&(typeof(this.level)==="number")&&(typeof(newName)==="string")&&(typeof(this.grade)==="number")){
+    if((this.level && newName &&this.grade)&&(typeof(this.level)==="number")&&
+    (typeof(newName)==="string")&&(typeof(this.grade)==="number")){
       if(this.grade<45){
-        return( 
-        `${this.name} cannot be promoted to the next level\nReason:${this.name}'s Score is below Average Performance\nAdvice:${this.name} should repeat the current level\nScore:${this.grade}\nLevel:${this.level}`)
+        return(`${this.name} is below average`)
       }
       if(45<this.grade && this.grade<=70){
-        return (
-        `${this.name} is Promoted\nReason:${this.name}'s Score is Above Average Performance\nAdvice:${this.name} can still do better\nScore:${this.grade}\nNew Level:${this.level+1}`)
+        return (`${this.name} is above average performance`)
       }
       if(70<this.grade && this.grade<100){
-        return (
-        `${this.name} is double Promoted\nReason:${this.name}'s Score exceeds expected Performance\nAdvice:${this.name} can do better if challenged\nScore:${this.grade}\nNew Level:${this.level+2}`)
+        return (`${this.name} is an excellent student`)
       }
     }else{
       return "`Level` `name` or `score` should be of `number` `string` or `number` type and should not be empty"
--- test/test-spec.js
+++ test/test-spec.js
@@ -1,41 +1,22 @@
 const chai = require("chai");
 const assert=chai.assert;
-const student= require("../oop.js")
 const gradeStudent= require("../oop.js")
 
-let checkError=new student.Student(" ", "2")
-let checkError2=new gradeStudent.GradeStudent("doe", 1, " ")
-let studentDetail=new student.Student("jane",1)
-let studentGrade=new gradeStudent.GradeStudent("doe", 1,79)
-// check if getStudent function accepts an invalid input
-describe("Invalid input(s):", function() {
-	describe("You have made an invalid input in getStudent function;", () => {
-		it("should return `Level` or `name` should be of `number` or `string` type and should not be empty", () => {
-			assert.equal(checkError.getStudent(), "`Level` or `name` should be of `number` or `string` type and should not be empty");
-		});
-	});
-})
-//check if the getGrade function accepts an invalid input
-describe("Invalid input(s):", function() {
-	describe("You have made an invalid input in gradeStudent function;", () => {
-		it("should return `Level` `name` or `score` should be of `number` `string` or `number` type and should not be empty", () => {
-			assert.equal(checkError2.getGrade(), "`Level` `name` or `score` should be of `number` `string` or `number` type and should not be empty");
-		});
-	});
-})
+let studentGrade=new gradeStudent.GradeStudent("doe", 1,59)
+let studentDetail=new gradeStudent.GradeStudent("doe", 1,59)
 //expect student's details
-describe("Student's details:", function() {
-	describe("Return the details of the student", () => {
-		it("should return the name and level of student", () => {
-			assert.equal(studentDetail.getStudent(), `Student's name:jane\nStudent's level:1`);
+describe("Student's details:", () => {
+	describe("If valid,", () => {
+		it("should return details of student", () => {
+			assert.equal(studentDetail.getDetails(),`doe is a level 1 student`);
 		});
 	});
 })
 //expect student's grade details
 describe("Student's grade details:", function() {
-	describe("Return grade details of the student;", () => {
+	describe("if valid,", () => {
 		it("should return the details of student's performance", () => {
-			assert.equal(studentGrade.getGrade(), `doe is double Promoted\nReason:doe's Score exceeds expected Performance\nAdvice:doe can do better if challenged\nScore:79\nNew Level:${1+2}`);
+			assert.equal(studentGrade.getGrade(), `doe is above average performance`);
 		});
 	});
 })
